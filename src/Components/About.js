bf6d154f8c76302d1efe20f7d2cb97c282e06fa6
junaidzamaahmed <junaidahmedzama@gmail.com>
2022-09-01 15:57:19 +0600

    last commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,7 +22,7 @@ const About = ({ data }) => {
           <img
             className="profile-pic"
             src={profilepic}
-            alt="Sonny's Profile Pic"
+            alt="Junaid's Profile Pic"
           />
         </div>
         <div className="nine columns main-col">
